398c66d69800e5681104dc98e29d5ebaa966f154
John Salazar Pinto <johnpiterzon@gmail.com>
2024-02-29 23:22:22 -0400

    register, login and refresh token implemented.

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -40,7 +40,16 @@ INSTALLED_APPS = [
     'rest_framework',
     'core',
     'core.user',
+    'core.auth',
 ]
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+   ),
+   'DEFAULT_FILTER_BACKENDS':
+        ['django_filters.rest_framework.DjangoFilterBackend'],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
